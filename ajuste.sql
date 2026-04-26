0488cfadbf7597dc73edf2566bcce8b0a188ba17
Kendall <kmgarior@gmail.com>
2026-04-26 01:09:27 -0600

    Update ajuste.sql
    Se agrega la consulta maestra

--- ajuste.sql
+++ ajuste.sql
@@ -408,3 +408,28 @@ SELECT fn_notario('gomitaofresa_baja_prioridad');
 SELECT * FROM logs_hashy;
 
 SELECT fn_gran_sello('gomitaofresa_baja_prioridad');
+
+--Consulta maestra 
+SELECT 
+    GROUP_CONCAT(
+        fn_gran_sello(
+            fn_notario(
+                fn_escultor(
+                    fn_purificador(nombre_sucio),
+                    fn_espia_tortuga(categoria, precio_finca)
+                )
+            )
+        )
+        ORDER BY id ASC
+        SEPARATOR ' # '
+    ) AS resultado_final_del_trio
+FROM inventario_pirata
+WHERE 
+    fn_cernidor(id) = TRUE
+    AND fn_reloj_arena(fecha_ingreso, meses_validez) = 'Fresco';
+
+
+
+SELECT * 
+FROM logs_hashy
+ORDER BY id;
